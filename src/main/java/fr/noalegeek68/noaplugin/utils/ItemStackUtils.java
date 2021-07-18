8ec3b416e50aa4a681aee964edaf9b42bb833342
NoaLeGeek68 <noacraft68@gmail.com>
2021-07-18 11:35:02 +0200

    Fixed functions in ItemStackUtils

--- src/main/java/fr/noalegeek68/noaplugin/utils/ItemStackUtils.java
+++ src/main/java/fr/noalegeek68/noaplugin/utils/ItemStackUtils.java
@@ -5,9 +5,10 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.Damageable;
 import org.bukkit.inventory.meta.ItemMeta;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
+import java.util.stream.Collectors;
 
 public class ItemStackUtils {
 
@@ -31,14 +32,14 @@ public class ItemStackUtils {
     }
 
     public static Material randomBanner(){
-        List<Material> banners = Arrays.stream(Material.values()).filter(banner -> banner.name().endsWith("_BANNER") && !banner.name().endsWith("_WALL_BANNER"))
+        List<Material> banners = Arrays.stream(Material.values()).filter(banner -> banner.name().endsWith("_BANNER") && !banner.name().endsWith("_WALL_BANNER") && !banner.name().startsWith("LEGACY"))
                 .collect(Collectors.toList());
         return banners.get(new Random().nextInt(banners.size()));
     }
 
     public static Material randomSkull(){
-        List<Material> skulls = Arrays.stream(Material.values()).filter(material -> (material.name().endsWith("_HEAD") && !material.name().endsWith("_WALL_HEAD")) ||
-                (material.name().endsWith("_SKULL") && !material.name().endsWith("_WALL_SKULL"))).collect(Collectors.toList());
+        List<Material> skulls = Arrays.stream(Material.values()).filter(material -> (!material.name().startsWith("PISTON") && material.name().endsWith("_HEAD") && !material.name().endsWith("_WALL_HEAD")) ||
+                (material.name().endsWith("_SKULL") && !material.name().startsWith("LEGACY") && !material.name().endsWith("_WALL_SKULL"))).collect(Collectors.toList());
         return skulls.get(new Random().nextInt(skulls.size()));
     }
 }
