38283ba03f27b9de42b3b1b6bba9f917c2dd8f98
NoaLeGeek68 <noacraft68@gmail.com>
2021-07-21 12:03:21 +0200

    Added a class and a function in this class (idk why)

---
+++ src/main/java/fr/noalegeek68/noaplugin/utils/Utils.java
@@ -0,0 +1,9 @@
+package fr.noalegeek68.noaplugin.utils;
+
+import java.util.Random;
+
+public class Utils {
+    public static int randomMinMax(int min, int max) {
+        return new Random().nextInt((max - min) + 1) + min;
+    }
+}
